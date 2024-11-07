a1c9a13590a49a0022e28ef2bf26677b2edea119
Max Podloobniy <max.podloobniy@gmail.com>
2024-11-07 21:49:14 +0200

    Traslate comments into English and make other fixes

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
-# Параметри
-DAYS = 10  # два тижні робочих днів
-HOURS_PER_DAY = 10  # навчання з 8:00 до 17:00
-START_HOUR = 8
+# Parameters
+DAYS = 10  # two weeks of working days
+START_HOUR = 9
+HOURS_PER_DAY = 7  # Study from 9:00 to 17:00
+MAX_SUBJECTS_PER_DAY = 6 # Required for programming restrictions
 SUBJECTS = [
     'English', 'Economics', 'Computer Systems', 'Data Science', 'Comp Vision',
     'Algorithms for electronic voting', 'Data science fundamentals', 'Network and protocols',
     'Intelligent transport systems', 'Basics of WEB technologies', 'Internet of things technologies',
     'Reactive programming', 'Engineering computer graphics', 'Simulation modelling'
 ]
-# Загальні предмети (відвідуються всіма групами одночасно)
-COMMON_SUBJECTS = ['English', 'Economics', 'Computer Systems', 'Data Science', 'Comp Vision',
-                   'Algorithms for electronic voting', 'Data science fundamentals', 'Network and protocols',
-                   'Intelligent transport systems', 'Basics of WEB technologies', 'Internet of things technologies',
-                   'Reactive programming', 'Engineering computer graphics', 'Simulation modelling']
+# General subjects (attended by all groups at the same time, it is necessary because there are lectures and practicals and laboratory work)
+COMMON_SUBJECTS = [
+    'English', 'Economics', 'Computer Systems', 'Data Science', 'Comp Vision',
+    'Algorithms for electronic voting', 'Data science fundamentals', 'Network and protocols',
+    'Intelligent transport systems', 'Basics of WEB technologies', 'Internet of things technologies',
+    'Reactive programming', 'Engineering computer graphics', 'Simulation modelling'
+]
--- main.py
+++ main.py
@@ -1,40 +1,33 @@
 from model_handler import add_all_constraints, minimize_slots_usage, add_subject_slots
 from data_loader import load_data_from_excel
-from visualizer import visualize_result
+from visualizer import visualize_result_full
 from ortools.sat.python import cp_model
 
 
 def main():
-    # Створення моделі
+    # Create a model
     model = cp_model.CpModel()
 
-    # Визначення предметів для кожної групи та їх мінімальна кількість годин
+    # Define the subjects for each group, theachers for each subject and their required number of hours
     teachers, subjects_per_group = load_data_from_excel('data/')
-    print('Teachers added')
 
-    # Додаємо змінні
+    # Add variables to model
     subject_slots = add_subject_slots(model, subjects_per_group)
-    print('Subjects added')
 
-    # Додаємо обмеження
+    # Add constrains to model
     add_all_constraints(model, subject_slots, subjects_per_group, teachers)
-    print('Constraints added')
 
-    # Додаємо функцію мінімізації
+    # Add minimization function
     minimize_slots_usage(model, subject_slots, subjects_per_group)
-    print('Minimize function added')
 
-    # Виклик розв'язувача
     schedule_solver = cp_model.CpSolver()
-    print('Solver created')
     status = schedule_solver.Solve(model)
 
-    # Вивід результату
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
         for group in subjects_per_group:
-            visualize_result(schedule_solver, subject_slots, subjects_per_group, teachers, group)
+            visualize_result_full(schedule_solver, subject_slots, subjects_per_group, teachers, group)
     else:
-        print("Рішення не знайдено")
+        print("No solution found")
 
 
 if __name__ == "__main__":
